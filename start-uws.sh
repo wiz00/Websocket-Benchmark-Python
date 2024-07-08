4484e5a5a6400bb4842852983654e1fcc5b3789e
wiz <alexx.wizard@gmail.com>
2024-07-08 18:04:09 +0200

    Updated startup scripts

--- start-uws.sh
+++ start-uws.sh
@@ -1,2 +1,2 @@
-docker build . -f Dockerfile.Uws -t websocket_benchmark/python-uws
+docker build . -f Dockerfile.Uws -t websocket_benchmark/python-uws && \
 docker run --rm -p 8080:8080 websocket_benchmark/python-uws
--- start-websockets.sh
+++ start-websockets.sh
@@ -1,2 +1,2 @@
-docker build . -f Dockerfile.Websockets -t websocket_benchmark/python-websockets
+docker build . -f Dockerfile.Websockets -t websocket_benchmark/python-websockets && \
 docker run --rm -p 8080:8080 websocket_benchmark/python-websockets
